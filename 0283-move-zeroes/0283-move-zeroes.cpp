9a7dffe0078aed980db47d85fcab40b31920cca9
Bereket.Tadiwos <86204534+BEKI77@users.noreply.github.com>
2023-10-24 20:42:57 +0300

    Time: 16 ms (72.41%), Space: 19.6 MB (25.58%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0){
+                count++;
+            }else if(count>0){
+                swap(nums[i],nums[i-count]);
+            }
+        }     
+    }
+};
