d0b25a56190c21020ca711421f5d5729864ad7da
Bereket.Tadiwos <86204534+BEKI77@users.noreply.github.com>
2023-11-02 21:44:45 +0300

    Time: 2 ms (98.69%), Space: 7.6 MB (60.80%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-            sort(s.begin(), s.end());
-            sort(t.begin(), t.end());
-        return s==t;
+        
+        if(s.length()!=t.length()) return false;
+        else{
+            int n = s.length();
+            vector<int> hash1(26,0),hash2(26,0);
+            for(int i=0; i<s.length(); i++){
+                hash1[s[i]-'a']++;
+                hash2[t[i]-'a']++;
+            }
+            if(hash1!=hash2) return false;
+        }
+            
+        return true;
     }
 };
