3691687d06dbb87172960f4b75f2ca33d20c9ae4
Bereket.Tadiwos <86204534+BEKI77@users.noreply.github.com>
2024-01-10 18:31:26 +0300

    Added solution for Find All Anagrams in a String

--- leetcode-solutions/find-all-anagrams-in-a-string.py
+++ leetcode-solutions/find-all-anagrams-in-a-string.py
@@ -1,10 +1,22 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        hash = Counter(p)
+        ha = [0]*26
+        dynamic = [0]*26
         ans = []
-        for i in range(len(s)-len(p)+1):
-            temp = Counter(s[i:i+len(p)])
-            if temp==hash:
-                ans.append(i)
+        if len(p)>len(s):
+            return ans
+        for i in range(len(p)):
+            ha[ord(p[i])-ord('a')]+=1
+            dynamic[ord(s[i])-ord('a')]+=1
+
+        if dynamic==ha:
+            ans.append(0)
+
+        for i in range(len(p),len(s)):
+            dynamic[ord(s[i])-ord('a')]+=1
+            dynamic[ord(s[i-len(p)])-ord('a')]-=1
+            if dynamic==ha:
+                ans.append(i-len(p)+1)
+        
         return ans
             
