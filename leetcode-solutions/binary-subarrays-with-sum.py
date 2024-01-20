3cb809d31e8d5478ffe1022c9b396d19ded936fa
Bereket.Tadiwos <86204534+BEKI77@users.noreply.github.com>
2024-01-20 14:15:53 +0300

    Added solution for Binary Subarrays With Sum

--- leetcode-solutions/binary-subarrays-with-sum.py
+++ leetcode-solutions/binary-subarrays-with-sum.py
@@ -1,25 +1,43 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        ans,rSum = 0,goal
-        l,r = 0,0
-        for r in range(len(nums)):
-            rSum-=nums[r]
-            while rSum<0:
-                rSum+=nums[l]
-                l+=1
-            ans += (r-l+1)
-        l,r = 0,0
-        lSum , res = goal-1,0
-        if lSum<0:
-            res=0
-        else:
-            for r in range(len(nums)):
-                lSum-=nums[r]
-                while lSum<0:
-                    lSum+=nums[l]
-                    l+=1
-                res +=(r-l+1)
-        return ans-res
+        mp = defaultdict(int)
+        mp[0]=1
+        sUm,ans = 0,0
+        for i in nums:
+            sUm+=i
+            if sUm-goal in mp:
+                ans+=mp[sUm-goal]
+            mp[sUm]+=1
+        return ans
+
+       
+       
+       
+       
+       
+       
+       
+       
+        # ans,rSum = 0,goal
+        # l,r = 0,0
+        # for r in range(len(nums)):
+        #     rSum-=nums[r]
+        #     while rSum<0:
+        #         rSum+=nums[l]
+        #         l+=1
+        #     ans += (r-l+1)
+        # l,r = 0,0
+        # lSum , res = goal-1,0
+        # if lSum<0:
+        #     res=0
+        # else:
+        #     for r in range(len(nums)):
+        #         lSum-=nums[r]
+        #         while lSum<0:
+        #             lSum+=nums[l]
+        #             l+=1
+        #         res +=(r-l+1)
+        # return ans-res
 
 
 # def atMost(S):
