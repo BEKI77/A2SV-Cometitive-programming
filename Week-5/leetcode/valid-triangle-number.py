211713eca802984bcb4aeaaf3db0f4dd974d195c
Bereket.Tadiwos <86204534+BEKI77@users.noreply.github.com>
2024-02-17 16:02:13 +0300

    Add solution for Valid Triangle Number

--- Week-5/leetcode/valid-triangle-number.py
+++ Week-5/leetcode/valid-triangle-number.py
@@ -1,12 +1,26 @@
-import bisect
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
-        nums.sort()
+        nums.sort(reverse = True)
         ans = 0
         for i in range(len(nums)):
-            temp1 = nums[i]
-            for j in range(i+1,len(nums)):
-                temp2 = nums[j]
-                k = bisect.bisect_left(nums[j+1:], temp2+temp1)
-                ans+=(k)
+            large = nums[i]
+            l,r = i+1, len(nums)-1
+            while l<r:
+                if large < nums[l]+nums[r]:
+                    ans+=(r-l)
+                    l+=1
+                else:
+                    r-=1
         return ans
+
+
+
+
+        # ans = 0
+        # for i in range(len(nums)):
+        #     temp1 = nums[i]
+        #     for j in range(i+1,len(nums)):
+        #         temp2 = nums[j]
+        #         k = bisect.bisect_left(nums[j+1:], temp2+temp1)
+        #         ans+=(k)
+        # return ans
